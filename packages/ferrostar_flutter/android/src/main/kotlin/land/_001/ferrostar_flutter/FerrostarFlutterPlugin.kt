baded38029058b3973fb85977530a672ddcf4fc1
cafca <cafca@001.land>
2026-04-16 23:46:29 +0200

    chore(plugin,android): stub plugin entry point, defer Ferrostar wiring

--- packages/ferrostar_flutter/android/src/main/kotlin/land/_001/ferrostar_flutter/FerrostarFlutterPlugin.kt
+++ packages/ferrostar_flutter/android/src/main/kotlin/land/_001/ferrostar_flutter/FerrostarFlutterPlugin.kt
@@ -6,30 +6,17 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-/** FerrostarFlutterPlugin */
-class FerrostarFlutterPlugin :
-    FlutterPlugin,
-    MethodCallHandler {
-    // The MethodChannel that will the communication between Flutter and native Android
-    //
-    // This local reference serves to register the plugin with the Flutter Engine and unregister it
-    // when the Flutter Engine is detached from the Activity
+// TODO(android): wire Ferrostar Kotlin SDK when targeting Android
+class FerrostarFlutterPlugin : FlutterPlugin, MethodCallHandler {
     private lateinit var channel: MethodChannel
 
-    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ferrostar_flutter")
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "land._001/ferrostar_flutter")
         channel.setMethodCallHandler(this)
     }
 
-    override fun onMethodCall(
-        call: MethodCall,
-        result: Result
-    ) {
-        if (call.method == "getPlatformVersion") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")
-        } else {
-            result.notImplemented()
-        }
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        result.notImplemented()
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {

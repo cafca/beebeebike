f0cb13a3748df903bc0946a13bbe1d21e54a9666
cafca <cafca@001.land>
2026-04-17 12:24:50 +0200

    chore(plugin,android): add seam stubs for future Android implementation

---
+++ packages/ferrostar_flutter/android/src/main/kotlin/land/_001/ferrostar_flutter/ControllerRegistry.kt
@@ -0,0 +1,4 @@
+package land._001.ferrostar_flutter
+
+// TODO(android): implement with Ferrostar NavigationController
+object ControllerRegistry
---
+++ packages/ferrostar_flutter/android/src/main/kotlin/land/_001/ferrostar_flutter/Serialization.kt
@@ -0,0 +1,4 @@
+package land._001.ferrostar_flutter
+
+// TODO(android): implement Dart <-> Ferrostar type conversions
+object Serialization

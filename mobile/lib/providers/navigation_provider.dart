2f771f9bf23830128c31c9688773e225028d663a
cafca <cafca@001.land>
2026-04-17 22:31:49 +0200

    feat(mobile): wire flutter_tts for spoken navigation instructions

--- mobile/lib/providers/navigation_provider.dart
+++ mobile/lib/providers/navigation_provider.dart
@@ -1,5 +1,6 @@
 import 'package:ferrostar_flutter/ferrostar_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:geolocator/geolocator.dart';
 
 import '../api/client.dart';
@@ -7,9 +8,12 @@ import '../api/routing_api.dart';
 import '../navigation/location_converter.dart';
 import '../navigation/navigation_service.dart';
 
+final flutterTtsProvider = Provider<FlutterTts>((ref) => FlutterTts());
+
 final navigationServiceProvider = Provider<NavigationService>((ref) {
   final dio = ref.watch(dioProvider);
   final routingApi = RoutingApi(dio);
+  final tts = ref.watch(flutterTtsProvider);
   return NavigationService(
     createController: (osrmJson, waypoints) =>
         FerrostarFlutter.instance.createController(
@@ -19,6 +23,6 @@ final navigationServiceProvider = Provider<NavigationService>((ref) {
     loadNavigationRoute: ({required origin, required destination}) =>
         routingApi.computeNavigationRoute(origin, destination),
     locationStream: Geolocator.getPositionStream().map(positionToUserLocation),
-    speakInstruction: (_) async {},
+    speakInstruction: (text) async { await tts.speak(text); },
   );
 });
